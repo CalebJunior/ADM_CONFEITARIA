8f276f59670e1558e9d2538469b1d6ff96ac951a
Caleb Junior <calebjunior37@gmail.com>
2022-05-03 11:55:33 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/example/app_firebase/Funcionario.java
@@ -0,0 +1,19 @@
+package com.example.app_firebase;
+
+public class Funcionario {
+    private String email;
+    private String password;
+
+    public Funcionario(){
+
+    }
+
+    public boolean validaFunc(String em, String pass){
+
+
+
+
+        return true;
+
+    }
+}
---
+++ app/src/main/java/com/example/app_firebase/Pedidos.java
@@ -0,0 +1,4 @@
+package com.example.app_firebase;
+
+public class Pedidos {
+}
